b6289c81e23859ee60d3c0e0abf0164cafe8fd27
Sujal Maiti <62564807+sujal111@users.noreply.github.com>
2022-11-13 18:59:34 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title("Hello streamlit")
+st.header("Financial Factors- Macroeconomics")
+
+
+st.subheader("Gross Domestic Product (GDP) Most Recent Report: Q2 GDP -0.6% (preliminary Grade: Bad")
+st.text("Like this video")
+st.markdown("""# h1 tag""")
